366a49b056429c6bbddbb12f840c12b0ff85e231
Jakub Kicinski <jakub.kicinski@netronome.com>
2017-12-27 18:39:07 -0800

    bpf: offload: free program id when device disappears
    Bound programs are quite useless after their device disappears.
    They are simply waiting for reference count to go to zero,
    don't list them in BPF_PROG_GET_NEXT_ID by freeing their ID
    early.
    
    Note that orphaned offload programs will return -ENODEV on
    BPF_OBJ_GET_INFO_BY_FD so user will never see ID 0.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Quentin Monnet <quentin.monnet@netronome.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit e82f1fef956a64bfdb13bc486f68d43defc8823e)

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -803,6 +803,50 @@ static void bpf_prog_uncharge_memlock(struct bpf_prog *prog)
 	free_uid(user);
 }
 
+<<<<<<< HEAD
+=======
+static int bpf_prog_alloc_id(struct bpf_prog *prog)
+{
+	int id;
+
+	spin_lock_bh(&prog_idr_lock);
+	id = idr_alloc_cyclic(&prog_idr, prog, 1, INT_MAX, GFP_ATOMIC);
+	if (id > 0)
+		prog->aux->id = id;
+	spin_unlock_bh(&prog_idr_lock);
+
+	/* id is in [1, INT_MAX) */
+	if (WARN_ON_ONCE(!id))
+		return -ENOSPC;
+
+	return id > 0 ? 0 : id;
+}
+
+void bpf_prog_free_id(struct bpf_prog *prog, bool do_idr_lock)
+{
+	/* cBPF to eBPF migrations are currently not in the idr store.
+	 * Offloaded programs are removed from the store when their device
+	 * disappears - even if someone grabs an fd to them they are unusable,
+	 * simply waiting for refcnt to drop to be freed.
+	 */
+	if (!prog->aux->id)
+		return;
+
+	if (do_idr_lock)
+		spin_lock_bh(&prog_idr_lock);
+	else
+		__acquire(&prog_idr_lock);
+
+	idr_remove(&prog_idr, prog->aux->id);
+
+	prog->aux->id = 0;
+	if (do_idr_lock)
+		spin_unlock_bh(&prog_idr_lock);
+	else
+		__release(&prog_idr_lock);
+}
+
+>>>>>>> e82f1fef956a (bpf: offload: free program id when device disappears)
 static void __bpf_prog_put_rcu(struct rcu_head *rcu)
 {
 	struct bpf_prog_aux *aux = container_of(rcu, struct bpf_prog_aux, rcu);
